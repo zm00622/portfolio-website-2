2c01ea6e356a9475fbe4c589bbaab14f04dfdb0b
zm00622 <zarymanning@gmail.com>
2020-02-16 22:25:44 -0500

    Updates

--- precache-manifest.be9c607d765a7f0d680eb463d1261bc0.js
+++ precache-manifest.be9c607d765a7f0d680eb463d1261bc0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9eb271b76a26744612ab59033ebe99f2",
+    "revision": "228ae9e20e2f95788d8d86d81ccfb4f3",
     "url": "/portfolio-website-2/index.html"
   },
   {
-    "revision": "d41d3a46818e4386bf22",
-    "url": "/portfolio-website-2/static/css/main.2c9bcc66.chunk.css"
+    "revision": "d241ce42795f0e9b8626",
+    "url": "/portfolio-website-2/static/css/main.0d3e1296.chunk.css"
   },
   {
-    "revision": "ef4a168f957666d71fd6",
-    "url": "/portfolio-website-2/static/js/2.2d107463.chunk.js"
+    "revision": "c015382690b37f6043eb",
+    "url": "/portfolio-website-2/static/js/2.03bde2c2.chunk.js"
   },
   {
     "revision": "2bcf73e3839e96bb488d3fc4da7ab52a",
-    "url": "/portfolio-website-2/static/js/2.2d107463.chunk.js.LICENSE.txt"
+    "url": "/portfolio-website-2/static/js/2.03bde2c2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d41d3a46818e4386bf22",
-    "url": "/portfolio-website-2/static/js/main.c1ad9312.chunk.js"
+    "revision": "d241ce42795f0e9b8626",
+    "url": "/portfolio-website-2/static/js/main.d8493685.chunk.js"
   },
   {
     "revision": "d0e8910c20f87317d9c7",
