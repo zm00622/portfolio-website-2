79755731848ed72d566c5e9664d0fb685092687c
zm00622 <zarymanning@gmail.com>
2020-02-16 19:19:55 -0500

    Updates

---
+++ precache-manifest.188436a9d10f81529e7421d53eab32cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b193651e30990f92edca0e79d008148",
+    "url": "/portfolio-website-2/index.html"
+  },
+  {
+    "revision": "d14da56bbee4614512da",
+    "url": "/portfolio-website-2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "17db94b7b8b55809cfa1",
+    "url": "/portfolio-website-2/static/js/2.b86e2b31.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio-website-2/static/js/2.b86e2b31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14da56bbee4614512da",
+    "url": "/portfolio-website-2/static/js/main.2f53b942.chunk.js"
+  },
+  {
+    "revision": "d0e8910c20f87317d9c7",
+    "url": "/portfolio-website-2/static/js/runtime-main.29bf2d69.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-website-2/static/media/logo.5d5d9eef.svg"
+  }
+]);
